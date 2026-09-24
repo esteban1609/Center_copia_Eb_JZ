aa50357454cbe7b30bb1c9652cb886207ae942fb
Esteban Barroso <estebanjose1609@gmail.com>
2026-09-24 16:42:17 -0300

    new mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Center_copia_EB_JZ.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
